32688f7210e5f78881606385c07a96e4a9d5d799
Reza Rabbani <reza.rabbani@agvisorpro.com>
2023-10-30 20:02:41 -0600

    Project reducer is added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import configureBugStore from "./store/configureStore";
 import { bugAdded, bugResolved } from "./store/bugs";
+import { projectAdded } from "./store/projects";
 
 
 const store = configureBugStore();
@@ -9,3 +10,8 @@ store.subscribe(() => {
 console.log(store.getState());
 
 store.dispatch(bugAdded({description: "First Bug"}));
+store.dispatch(projectAdded({title: "First project"}));
+store.dispatch(bugAdded({description: "Second Bug"}));
+store.dispatch(projectAdded({title: "Second project"}));
+store.dispatch(bugAdded({description: "Third Bug"}));
+store.dispatch(bugResolved({id: 2}));
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import bugReducer from "./bugs"
+import reducer from "./reducer";
 
 
 export default function () {
-    return configureStore({reducer: bugReducer});
+    return configureStore({reducer: reducer});
 }
